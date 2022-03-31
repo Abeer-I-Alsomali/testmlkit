cadce223583a41c8d67c5cd872a88b76f6886c0d
Aqeel Assif <aqeel.assif.1@aramco.com.sa>
2022-03-31 13:33:34 +0300

    greenmask

--- app/src/main/java/com/example/testmlkit/GreenMasking.java
+++ app/src/main/java/com/example/testmlkit/GreenMasking.java
@@ -1,34 +1,44 @@
 package com.example.testmlkit;
 
-        import android.Manifest;
-        import android.annotation.SuppressLint;
-        import android.app.Activity;
-        import android.content.ActivityNotFoundException;
-        import android.content.Intent;
-        import android.content.pm.PackageManager;
-        import android.graphics.Bitmap;
-        import android.graphics.Color;
-        import android.graphics.Paint;
-        import android.graphics.Point;
-        import android.os.Bundle;
-        import android.provider.MediaStore;
-        import android.util.Log;
-        import android.view.View;
-        import android.view.Window;
-        import android.view.WindowManager;
-        import android.widget.Button;
-        import android.widget.ImageView;
-        import androidx.core.app.ActivityCompat;
-        import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-        import org.opencv.android.BaseLoaderCallback;
-        import org.opencv.android.LoaderCallbackInterface;
-        import org.opencv.android.OpenCVLoader;
-        import org.opencv.android.Utils;
-        import org.opencv.core.Core;
-        import org.opencv.core.Mat;
-        import org.opencv.core.Scalar;
-        import org.opencv.imgproc.Imgproc;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class GreenMasking extends Activity {
     private static final String TAG = "opencvtest";
@@ -40,6 +50,7 @@ public class GreenMasking extends Activity {
     private ImageView ResultImage;
 
     Mat unmaskedimageRGB;
+    Mat unmaskedimageBGR;
     Mat unmaskedimageHSV;
     Mat Mask;
     Mat Matresult ;
@@ -61,20 +72,37 @@ public class GreenMasking extends Activity {
         setContentView(R.layout.activity_green_masking);
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 == PackageManager.PERMISSION_DENIED)
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 01);
+              ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 01);
+
+        if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_DENIED)
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 02);
+
+
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 02);
+
 
         ButtonTakeImage = findViewById(R.id.takeimage);
-        Croppedimageview = findViewById(R.id.croppedimage);
+        Croppedimageview = findViewById(R.id.maskedImage);
         cvimageview=findViewById(R.id.cvimage);
         ResultImage=findViewById(R.id.maskedImage);
 
+
         ButtonTakeImage.setOnClickListener(v -> {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            try {
-                startActivityForResult(takePictureIntent, CameraIntent);
-            } catch (ActivityNotFoundException e) {
-                // display error state to the user
-            }
+
+            Drawable drawable = ResourcesCompat.getDrawable(getResources(),R.drawable.g2,null);
+            Bitmap imagebitmap  = ((BitmapDrawable) drawable).getBitmap();
+
+            greenMasking(imagebitmap);
+
+//            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//            try {
+//                startActivityForResult(takePictureIntent, CameraIntent);
+//            } catch (ActivityNotFoundException e) {
+//                // display error state to the user
+//            }
         });
 
 
@@ -91,7 +119,12 @@ public class GreenMasking extends Activity {
             Bundle extras = data.getExtras();
             Imagebitmap = (Bitmap) extras.get("data");
             cvimageview.setImageBitmap(Imagebitmap);
-            greenMasking(Imagebitmap);
+            Drawable drawable = ResourcesCompat.getDrawable(getResources(),R.drawable.g1,null);
+            Bitmap imagebitmap  = ((BitmapDrawable) drawable).getBitmap();
+         greenMasking(imagebitmap);
+
+
+        //    greenMasking(Imagebitmap);
 
 
         }
@@ -106,6 +139,7 @@ public class GreenMasking extends Activity {
                 {
                     Log.i("OpenCV", "OpenCV loaded successfully");
                      unmaskedimageRGB=new Mat();
+                      unmaskedimageBGR=new Mat();
                      unmaskedimageHSV=new Mat();
                      Mask=new Mat();
                      Matresult =new Mat();
@@ -132,34 +166,118 @@ public class GreenMasking extends Activity {
 
     private void greenMasking(Bitmap imagebitmap) {
 
+
+       Bitmap resultimage=imagebitmap;
+
+
         // Matrix
         unmaskedimageRGB=new Mat();
+        unmaskedimageBGR=new Mat();
         unmaskedimageHSV=new Mat();
         Mask=new Mat();
         Matresult =new Mat();
 
+
+
+        unmaskedimageBGR = Imgcodecs.imread(getResources().getDrawable(R.drawable.g2).toString());
+
+        imagesave(imagebitmap);
+
         //convert image to bitmap
-        Utils.bitmapToMat(imagebitmap,unmaskedimageRGB);
+        Utils.bitmapToMat(imagebitmap,unmaskedimageBGR);
+
+        //convert matrix from bgr to rgb
+        Imgproc.cvtColor(unmaskedimageBGR,unmaskedimageRGB,Imgproc.COLOR_BGR2RGB);
+
 
         //convert matrix from rgb to hsv
         Imgproc.cvtColor(unmaskedimageRGB, unmaskedimageHSV, Imgproc.COLOR_RGB2HSV_FULL);
 
+        Core.inRange(unmaskedimageHSV,new Scalar(40, 40, 40),new Scalar(70, 255, 255), Mask);
 
-        Core.inRange(unmaskedimageHSV, new Scalar(40, 40, 40), new Scalar(70, 255, 255), Mask);
+        Core.bitwise_and(unmaskedimageRGB,unmaskedimageRGB,Matresult,Mask);
 
-        Core.bitwise_and(unmaskedimageHSV, Matresult, Mask);
 
-        Bitmap Bitimageresult=null;
+        Utils.matToBitmap(Matresult,resultimage);
 
-        Utils.matToBitmap(Matresult,Bitimageresult);
+        if(resultimage!=null)
+            ResultImage.setImageBitmap(resultimage);
 
-        if(Bitimageresult!=null)
-            ResultImage.setImageBitmap(Bitimageresult);
+
+        Imgproc.cvtColor(Matresult, Matresult,Imgproc.COLOR_BGR2GRAY);
+
+        int size= (int) Matresult.total();
+
+        int precentage =(Core.countNonZero(Matresult)*100)/size;
+
+        Log.i("Matsize", String.valueOf(Core.countNonZero(Matresult)));
+        Log.i("Matsize", String.valueOf(size));
+
+
+
 
 
 
 
     }
+
+
+    public void imagesave(Bitmap bitmap){
+
+        FileOutputStream fos= null;
+        File file = getDisc();
+        if(!file.exists() && !file.mkdirs()) {
+            //Toast.makeText(this, "Can't create directory to store image", Toast.LENGTH_LONG).show();
+            //return;
+            Log.i("file","file not created");
+            return;
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyymmsshhmmss");
+        String date = simpleDateFormat.format(new Date());
+        String name = "FileName"+date+".jpg";
+        String file_name = file.getAbsolutePath()+"/"+name;
+        File new_file = new File(file_name);
+        Log.i("file","new_file created");
+        try {
+            fos= new FileOutputStream(new_file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            Toast.makeText(this, "Save success", Toast.LENGTH_LONG).show();
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.i("file","FNF");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void refreshGallery(File file){
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intent.setData(Uri.fromFile(file));
+        sendBroadcast(intent);
+    }
+
+    private File getDisc(){
+        String t= getCurrentDateAndTime();
+        File file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        return new File(file, "ImageDemo");
+    }
+
+    private String getCurrentDateAndTime() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        String formattedDate = df.format(c.getTime());
+        return formattedDate;
+
+    }
+
+
+
+
+
 
 
 }
--- app/src/main/java/com/example/testmlkit/opencvtestImage.java
+++ app/src/main/java/com/example/testmlkit/opencvtestImage.java
@@ -7,15 +7,10 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -26,24 +21,15 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 
-import org.jetbrains.annotations.NotNull;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
 
-import java.util.List;
-
-public class opencvtestImage extends Activity implements OnTouchListener{
+public class opencvtestImage extends Activity {
     private static final String  TAG              = "opencvtest";
     private static final int  CameraIntent              = 01;
 
@@ -94,7 +80,7 @@ public class opencvtestImage extends Activity implements OnTouchListener{
         });
         cvimageview = findViewById(R.id.cvimage);
 
-        cvimageview.setOnTouchListener(this);
+     ///   cvimageview.setOnTouchListener(this);
 
     }
 
@@ -105,7 +91,7 @@ public class opencvtestImage extends Activity implements OnTouchListener{
                 case LoaderCallbackInterface.SUCCESS:
                 {
                     Log.i(TAG, "OpenCV loaded successfully");
-                    cvimageview.setOnTouchListener(opencvtestImage.this::onTouch);
+                    //cvimageview.setOnTouchListener(opencvtestImage.this::onTouch);
                 } break;
                 default:
                 {
@@ -135,135 +121,139 @@ public class opencvtestImage extends Activity implements OnTouchListener{
             Bundle extras = data.getExtras();
             Imagebitmap = (Bitmap) extras.get("data");
             Drawable drawable = ResourcesCompat.getDrawable(getResources(),R.drawable.g1,null);
-            Imagebitmap  = ((BitmapDrawable) drawable).getBitmap();
+           // Imagebitmap  = ((BitmapDrawable) drawable).getBitmap();
             cvimageview.setImageBitmap(Imagebitmap);
+
             Flag=true;
-            onImageViewStarted();
+      //      onImageViewStarted();
 
         }
     }
 
 
 
-
-
-    public void onImageViewStarted() {
-       // mRgba = new Mat();
-        mDetector = new ColorBlobDetector();
-        mSpectrum = new Mat();
-        mBlobColorRgba = new Scalar(255);
-        mBlobColorHsv = new Scalar(255);
-        SPECTRUM_SIZE = new Size(200, 64);
-        CONTOUR_COLOR = new Scalar(255,0,0,255);
-    }
-
-
-
-
-    private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
-        Mat pointMatRgba = new Mat();
-        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
-        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
-        return new Scalar(pointMatRgba.get(0, 0));
-    }
-
-
-
-    public String classify(@NotNull Scalar c)
-    {
-
-        double hue = c.val[0];
-        double sat = c.val[1];
-        double lgt = c.val[2];
-
-        Log.i("color0",hue+"");
-        Log.i("color1",sat+"");
-        Log.i("color2",lgt+"");
-
-        //   if (lgt < 0.2)  return "Blacks";
-        //     if (lgt > 0.8)  return "Whites";
-///
-        ///if (sat < 0.25) return "Grays";
-
-        ///     if (hue < 30)   return "Reds";
-
-        if (hue >25)  return "Greens";
-        if (hue < 210)  return "Cyans";
-        if (hue < 270)  return "Blues";
-        if (hue < 330)  return "Magentas";
-        return "Reds";
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        mRgba=new Mat();
-        Utils.bitmapToMat(Imagebitmap,mRgba);
-        // mRgba = inputFrame.rgba();
-
-        Log.i("cameraframe","here");
-
-        if(Flag){
-            if (mIsColorSelected) {
-                mDetector.process(mRgba);
-                List<MatOfPoint> contours = mDetector.getContours();
-                Log.e(TAG, "Contours count: " + contours.size());
-                Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
-
-                Mat colorLabel = mRgba.submat(4, 68, 4, 68);
-                colorLabel.setTo(mBlobColorRgba);
-
-                Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
-                mSpectrum.copyTo(spectrumLabel);
-            }
-
-            int cols = mRgba.cols();
-            int rows = mRgba.rows();
-
-
-
-            int xOffset = (cvimageview.getWidth() - cols) / 2;
-            int yOffset = (cvimageview.getHeight() - rows) / 2;
-
-            int x = (int)event.getX() - xOffset;
-            int y = (int)event.getY() - yOffset;
-            Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
-
-            if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
-
-            Rect touchedRect = new Rect();
-
-            touchedRect.x = (x>4) ? x-4 : 0;
-            touchedRect.y = (y>4) ? y-4 : 0;
-
-            touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
-            touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
-
-            Mat touchedRegionRgba = mRgba.submat(touchedRect);
-
-            Mat touchedRegionHsv = new Mat();
-            Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
-
-            // Calculate average color of touched region
-            mBlobColorHsv = Core.sumElems(touchedRegionHsv);
-            int pointCount = touchedRect.width*touchedRect.height;
-            for (int i = 0; i < mBlobColorHsv.val.length; i++)
-                mBlobColorHsv.val[i] /= pointCount;
-
-            mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv);
-
-            Log.i(TAG, "Touched rgba color: (" + mBlobColorRgba.val[0] + ", " + mBlobColorRgba.val[1] +
-                    ", " + mBlobColorRgba.val[2] + ", " + mBlobColorRgba.val[3] + ")");
-
-            mDetector.setHsvColor(mBlobColorHsv);
-
-            Log.i("color",classify(mBlobColorHsv));
-            Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE, 0, 0, Imgproc.INTER_LINEAR_EXACT);
-            mIsColorSelected = true;
-
-            touchedRegionRgba.release();
-            touchedRegionHsv.release();
-
-        }
-        return false; // don't need subsequent touch events
-    }
+//
+//
+//    public void onImageViewStarted() {
+//       // mRgba = new Mat();
+//        mDetector = new ColorBlobDetector();
+//        mSpectrum = new Mat();
+//        mBlobColorRgba = new Scalar(255);
+//        mBlobColorHsv = new Scalar(255);
+//        SPECTRUM_SIZE = new Size(200, 64);
+//        CONTOUR_COLOR = new Scalar(255,0,0,255);
+//    }
+//
+//
+//
+//
+//    private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
+//        Mat pointMatRgba = new Mat();
+//        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
+//        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
+//        return new Scalar(pointMatRgba.get(0, 0));
+//    }
+//
+//
+//
+//    public String classify(@NotNull Scalar c)
+//    {
+//
+//        double hue = c.val[0];
+//        double sat = c.val[1];
+//        double lgt = c.val[2];
+//
+//        Log.i("color0",hue+"");
+//        Log.i("color1",sat+"");
+//        Log.i("color2",lgt+"");
+//
+//        //   if (lgt < 0.2)  return "Blacks";
+//        //     if (lgt > 0.8)  return "Whites";
+/////
+//        ///if (sat < 0.25) return "Grays";
+//
+//        ///     if (hue < 30)   return "Reds";
+//
+//        if (sat> 25 && lgt > 25){
+//        if (hue >49 && hue < 125)  return "Greens";
+//        }
+//        if (hue < 210)  return "Cyans";
+//        if (hue < 270)  return "Blues";
+//        if (hue < 330)  return "Magentas";
+//        return "Reds";
+//    }
+//
+//    @Override
+//    public boolean onTouch(View v, MotionEvent event) {
+//        mRgba=new Mat();
+//        Imagebitmap=((BitmapDrawable)cvimageview.getDrawable()).getBitmap();
+//        Utils.bitmapToMat(Imagebitmap,mRgba);
+//        // mRgba = inputFrame.rgba();
+//
+//        Log.i("cameraframe","here");
+//
+//        if(Flag){
+//            if (mIsColorSelected) {
+//                mDetector.process(mRgba);
+//                List<MatOfPoint> contours = mDetector.getContours();
+//                Log.e(TAG, "Contours count: " + contours.size());
+//                Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
+//
+//                Mat colorLabel = mRgba.submat(4, 68, 4, 68);
+//                colorLabel.setTo(mBlobColorRgba);
+//
+//                Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
+//                mSpectrum.copyTo(spectrumLabel);
+//            }
+//
+//            int cols = mRgba.cols();
+//            int rows = mRgba.rows();
+//
+//
+//
+//            int xOffset = (cvimageview.getWidth() - cols) / 2;
+//            int yOffset = (cvimageview.getHeight() - rows) / 2;
+//
+//            int x = (int)event.getX() - xOffset;
+//            int y = (int)event.getY() - yOffset;
+//            Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
+//
+//            if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+//
+//            Rect touchedRect = new Rect();
+//
+//            touchedRect.x = (x>4) ? x-4 : 0;
+//            touchedRect.y = (y>4) ? y-4 : 0;
+//
+//            touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
+//            touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
+//
+//            Mat touchedRegionRgba = mRgba.submat(touchedRect);
+//
+//            Mat touchedRegionHsv = new Mat();
+//            Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+//
+//            // Calculate average color of touched region
+//            mBlobColorHsv = Core.sumElems(touchedRegionHsv);
+//            int pointCount = touchedRect.width*touchedRect.height;
+//            for (int i = 0; i < mBlobColorHsv.val.length; i++)
+//                mBlobColorHsv.val[i] /= pointCount;
+//
+//            mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv);
+//
+//            Log.i(TAG, "Touched rgba color: (" + mBlobColorRgba.val[0] + ", " + mBlobColorRgba.val[1] +
+//                    ", " + mBlobColorRgba.val[2] + ", " + mBlobColorRgba.val[3] + ")");
+//
+//            mDetector.setHsvColor(mBlobColorHsv);
+//
+//            Log.i("color",classify(mBlobColorHsv));
+//            Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE, 0, 0, Imgproc.INTER_LINEAR_EXACT);
+//            mIsColorSelected = true;
+//
+//            touchedRegionRgba.release();
+//            touchedRegionHsv.release();
+//
+//        }
+//        return false; // don't need subsequent touch events
+//    }
 }
